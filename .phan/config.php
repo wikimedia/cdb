7223032ec9a3d98df9b29ebd4f1099a100c5a21a
Umherirrender <umherirrender_de.wp@web.de>
2025-07-13 20:12:25 +0200

    build: Updating mediawiki/mediawiki-phan-config to 0.16.0
    Change-Id: Ifc810d4480bd3f0ca310b84f1c4c01d5daf25c81

--- .phan/config.php
+++ .phan/config.php
@@ -1,2 +1,6 @@
 <?php
-return require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
+
+$cfg['exception_classes_with_optional_throws_phpdoc'][] = \Cdb\Exception::class;
+
+return $cfg;
