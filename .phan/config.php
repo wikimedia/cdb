3c76a3c38298ecd1c0ff11185b844df79233f8ce
Umherirrender <umherirrender_de.wp@web.de>
2026-04-03 21:29:21 +0200

    Declare strict types on all php files
    Require strict types on function calls to php internal functions or
    on function calls within the library.
    
    Bug: T422722
    Change-Id: I0b68126affa3eb085240bb177cec198e250368a9

--- .phan/config.php
+++ .phan/config.php
@@ -1,4 +1,6 @@
 <?php
+declare( strict_types = 1 );
+
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
 
 $cfg['exception_classes_with_optional_throws_phpdoc'][] = \Cdb\Exception::class;
